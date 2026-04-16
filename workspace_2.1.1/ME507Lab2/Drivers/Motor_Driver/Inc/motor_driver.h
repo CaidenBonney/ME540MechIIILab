e44ca1d104d24255c5241cd510eb33bc20f45c59
Caiden Bonney <caiden.bonney@gmail.com>
2026-04-16 14:49:39 -0700

    Lab2 Wrote Driver Haven't Tested

---
+++ workspace_2.1.1/ME507Lab2/Drivers/Motor_Driver/Inc/motor_driver.h
@@ -0,0 +1,19 @@
+#ifndef MOTOR_DRIVER_H
+#define MOTOR_DRIVER_H
+
+#include <stdio.h>
+
+typedef struct {
+    TIM_HandleTypeDef* htim;
+    uint32_t Channel1;
+    uint32_t Channel2;
+    bool enabled;
+    int8_t velocity;
+} motor_driver_t;
+
+void motor_driver_init(motor_driver_t* motor_driver, TIM_HandleTypeDef* htim, uint32_t Channel1, uint32_t Channel2,  bool enabled, int8_t velocity);
+void motor_driver_enable(motor_driver_t* motor_driver);
+void motor_driver_disable(motor_driver_t* motor_driver);
+void motor_driver_set_velocity(motor_driver_t* motor_driver, int8_t velocity);
+
+#endif
